4f434dd2b4bf18b2add944fd900c2ba98987bec8
heikkilotjonen <heikkimlotjonen@gmail.com>
2025-11-04 13:44:53 +0200

    Pelaajalistan refaktorointi

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,29 +1,35 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+    
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.player_reader = player_reader
+    
+    def top_scorers_by_nationality(self, nationality):
+        players = self.player_reader.get_players()
+        players = [p for p in players if p.nationality == nationality]
+        return players.sort(key=lambda p: p.goals + p.assists, reverse=True)  
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    print("Oliot:")
-
-    copy = []
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
-        if player.nationality == "FIN":
-            copy.append(player)
-    copy.sort(key=lambda p: p.goals + p.assists, reverse=True)
-    for player in copy:
         print(player)
-
+        
 if __name__ == "__main__":
     main()
